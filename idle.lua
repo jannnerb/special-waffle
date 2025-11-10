584d0f59e60464c69d451ee520e67cc11b0ea4ce
Jannnab <velocitygamer.wvu@gmail.com>
2025-11-10 05:55:48 +0500

    Create idle.lua

---
+++ idle.lua
@@ -0,0 +1,4 @@
+-- // converted using mech's animoduler
+local cfr = CFrame.new
+
+return {{tm=0;["Torso"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Head"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Left Leg"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Right Arm"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Right Leg"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};["Left Arm"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};};{tm=.267;["Head"]={cf=cfr(0,0,0,-.97,-.242,0,.242,-.97,0,0,0,1);es="Linear";ed="In";};};{tm=.333;["Head"]={cf=cfr(0,0,0,-.941,.34,0,-.34,-.941,0,0,0,1);es="Linear";ed="In";};};{tm=.583;["Head"]={cf=cfr(0,0,0,1,0,0,0,1,0,0,0,1);es="Linear";ed="In";};};}
